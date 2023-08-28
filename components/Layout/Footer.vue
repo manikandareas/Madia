830667fac04ec64888d87ab3b391b05f99849f72
manik <manik@localhost.localdomain>
2023-08-28 12:17:34 +0800

    ✨ adding some icons

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -2,16 +2,8 @@
   <footer class="bg-white rounded-lg shadow dark:bg-black mx-auto">
     <div class="md:min-w-[1200px] w-full max-w-screen-xl mx-auto p-4 md:py-8">
       <div class="sm:flex sm:items-center sm:justify-between">
-        <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            class="h-8 mr-3"
-            alt="Flowbite Logo"
-          />
-          <span
-            class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-            >Madia</span
-          >
+        <a href="#" class="flex items-center mb-4 sm:mb-0">
+          <icons-madia-white />
         </a>
         <ul
           class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400"
@@ -35,9 +27,8 @@
       />
       <span
         class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"
-        >© 2023
-        <a href="https://flowbite.com/" class="hover:underline">Madia</a>. All
-        Rights Reserved.</span
+        >© 2023 <a href="#" class="hover:underline">Madia</a>. All Rights
+        Reserved.</span
       >
     </div>
   </footer>
--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -3,16 +3,8 @@
     <div
       class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
     >
-      <a href="https://flowbite.com/" class="flex items-center">
-        <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          class="h-8 mr-3"
-          alt="Flowbite Logo"
-        />
-        <span
-          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-          >Madia</span
-        >
+      <a href="#" class="flex items-center">
+        <IconsMadiaGreen />
       </a>
       <div class="flex md:order-2">
         <!-- <button
